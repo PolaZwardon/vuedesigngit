254641d509426521c650c9d35e9cf679efcb3e89
PolaZwardon <pola.zwardon@gmail.com>
2020-07-28 08:26:08 +0200

    Fixed sizing

--- src/components/Adv.vue
+++ src/components/Adv.vue
@@ -55,10 +55,8 @@ export default {
         margin: 7px 7px;
         border-radius: 10%;
         position: relative;
-        background: url('./../assets/sushi.png') fixed top center;
-        background-size: 690px 500px;
-
-
+        background: url('./../assets/sushi.png') fixed center top;
+        background-size: 800px;
     }
     .diamond:hover{
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
